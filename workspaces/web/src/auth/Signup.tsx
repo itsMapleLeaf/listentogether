c614756d5cf566f452bfcb1433f9e97fdcc7402f
kingdaro <kingdaro@gmail.com>
2019-12-22 20:33:01 -0600

    functional-ish signup form

--- workspaces/web/src/auth/Signup.tsx
+++ workspaces/web/src/auth/Signup.tsx
@@ -1,28 +1,55 @@
+import { Form, Formik } from "formik"
 import React from "react"
+import { object, string } from "yup"
 import { useRoutes } from "../navigation/routerContext"
+import FormTextInput from "../ui/FormTextInput"
 
-type Props = {}
+type FormValues = {
+  name: string
+  email: string
+  password: string
+}
 
-function Signup(props: Props) {
-  const routes = useRoutes()
+let initialValues: FormValues = {
+  name: "",
+  email: "",
+  password: "",
+}
+
+let validationSchema = object<FormValues>({
+  name: string()
+    .max(15)
+    .required(),
+  email: string()
+    .email()
+    .required(),
+  password: string()
+    .min(8)
+    .required(),
+})
+
+function Signup() {
+  let routes = useRoutes()
+
+  let handleSubmit = (values: FormValues) => {
+    console.log(values)
+  }
+
   return (
     <>
       <h1>sign up</h1>
-      <form>
-        <label>
-          <div>name</div>
-          <input type="text" />
-        </label>
-        <label>
-          <div>email</div>
-          <input type="email" />
-        </label>
-        <label>
-          <div>password</div>
-          <input type="password" />
-        </label>
-        <button type="submit">submit</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <FormTextInput name="name" label="name" />
+          <FormTextInput name="email" label="email" />
+          <FormTextInput name="password" label="password" />
+          <button type="submit">submit</button>
+        </Form>
+      </Formik>
       <a {...routes.home.link()}>return to home</a>
     </>
   )
---
+++ workspaces/web/src/ui/FormTextInput.tsx
@@ -0,0 +1,19 @@
+import { useField } from "formik"
+import React from "react"
+
+type FormTextInputProps = React.ComponentPropsWithoutRef<"input"> & {
+  label?: string
+}
+
+export default function FormTextInput({ label, ...props }: FormTextInputProps) {
+  let [field, meta] = useField(props as any)
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <br />
+      <input {...field} {...props} />
+      <br />
+      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+    </div>
+  )
+}
