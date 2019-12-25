35a16b0b91397de344a5fa8bcbc2ce6930a7a8c3
kingdaro <kingdaro@gmail.com>
2019-12-25 15:12:23 -0600

    new frontend

--- workspaces/web/src/index.tsx
+++ workspaces/web/src/index.tsx
@@ -1,24 +1,5 @@
-import { ApolloProvider } from "@apollo/react-hooks"
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./app/App"
-import { RootStore } from "./RootStore"
-import { RootStoreProvider } from "./rootStoreContext"
-import "./ui/index.css"
+import App from "./App"
 
-async function main() {
-  const store = new RootStore()
-
-  store.auth.getAuthState()
-
-  ReactDOM.render(
-    <ApolloProvider client={store.apolloClient}>
-      <RootStoreProvider store={store}>
-        <App />
-      </RootStoreProvider>
-    </ApolloProvider>,
-    document.getElementById("root"),
-  )
-}
-
-main().catch(console.error)
+ReactDOM.render(<App />, document.getElementById("root"))
