e515fc1e0d7c5dfea7471b23cacc271b0120f34c
kingdaro <kingdaro@gmail.com>
2019-12-25 15:31:24 -0600

    super simple auth test

--- workspaces/web/src/App.tsx
+++ workspaces/web/src/App.tsx
@@ -1,9 +1,57 @@
-import React from "react"
+import createAuth0Client from "@auth0/auth0-spa-js"
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client"
+import React, { useEffect, useState } from "react"
 
 type Props = {}
 
 function App(props: Props) {
-  return <p>hi, i'm App</p>
+  const [client, setClient] = useState<Auth0Client>()
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    async function createClient() {
+      const client = await createAuth0Client({
+        domain: "kingdaro.auth0.com",
+        client_id: "tD5PbNq3BlwfPSLhuAbV6DGvaON7Q6F1",
+        redirect_uri: "http://localhost:3000/auth/callback",
+      })
+
+      if (window.location.pathname.startsWith(`/auth/callback`)) {
+        await client.handleRedirectCallback()
+        window.history.replaceState(undefined, document.title, "/")
+      }
+
+      const user = await client.getUser()
+      if (user) setUser(user)
+
+      setClient(client)
+    }
+
+    createClient()
+  }, [])
+
+  return (
+    <main>
+      {client ? (
+        user ? (
+          <>
+            <code>{JSON.stringify(user)}</code>
+            <button
+              onClick={() =>
+                client.logout({ returnTo: window.location.origin })
+              }
+            >
+              log out
+            </button>
+          </>
+        ) : (
+          <button onClick={() => client.loginWithRedirect()}>log in</button>
+        )
+      ) : (
+        <p>loading client...</p>
+      )}
+    </main>
+  )
 }
 
 export default App
