8b0adbc446890b6505de37e3623a003b390a42b8
kingdaro <kingdaro@gmail.com>
2019-12-25 23:24:29 -0600

    disable button while creating room

--- workspaces/web/src/app/Home.tsx
+++ workspaces/web/src/app/Home.tsx
@@ -1,22 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import { AuthUser } from "../auth/useAuth"
 import { createApi } from "../network/api"
 
 type Props = { user: AuthUser; token: string; onLogout: () => void }
 
 function Home({ user, token, onLogout }: Props) {
+  const [loading, setLoading] = useState(false)
   const api = createApi(token)
 
   const createRoom = async () => {
+    setLoading(true)
     console.log(await api.createRoom())
+    setLoading(false)
     // redirect to room url
   }
 
   return (
     <main>
       <p>hi, {user.name}! what would you like to do?</p>
-      <button onClick={createRoom}>create room</button>
-      <button onClick={onLogout}>log out</button>
+      <button disabled={loading} onClick={createRoom}>
+        create room
+      </button>
+      <button disabled={loading} onClick={onLogout}>
+        log out
+      </button>
     </main>
   )
 }
