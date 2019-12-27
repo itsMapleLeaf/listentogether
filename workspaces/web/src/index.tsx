6a979bc836545aca0e4a2a609fc1bf865c322d73
kingdaro <kingdaro@gmail.com>
2019-12-27 15:57:02 -0600

    add apollo provider jdfklsfjdks

--- workspaces/web/src/index.tsx
+++ workspaces/web/src/index.tsx
@@ -1,11 +1,19 @@
+import { ApolloProvider } from "@apollo/react-hooks"
+import ApolloClient from "apollo-boost"
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./app/App"
 
+const client = new ApolloClient({
+  uri: `http://localhost:4000`,
+})
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById("root"),
 )
