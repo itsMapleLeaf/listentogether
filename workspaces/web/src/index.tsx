56cc02edb85a15ca7411b1a6d2c5680e963eebd3
kingdaro <kingdaro@gmail.com>
2019-12-28 20:57:39 -0600

    remove apollo client

--- workspaces/web/src/index.tsx
+++ workspaces/web/src/index.tsx
@@ -1,15 +1,11 @@
-import { ApolloProvider } from "@apollo/react-hooks"
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
-import { client } from "./apolloClient"
 import App from "./app/App"
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root"),
 )
