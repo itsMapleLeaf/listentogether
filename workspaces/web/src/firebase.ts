d432d091b8af8593e4bcf8d99322aee3d5a02e18
kingdaro <kingdaro@gmail.com>
2019-12-28 20:39:17 -0600

    configure firebase

---
+++ workspaces/web/src/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/analytics"
+import "firebase/firestore"
+
+const config = {
+  apiKey: "AIzaSyA-eIOZtddutNeh5KMTBifIGpqxnIZHUEM",
+  authDomain: "listen-together-637e4.firebaseapp.com",
+  databaseURL: "https://listen-together-637e4.firebaseio.com",
+  projectId: "listen-together-637e4",
+  storageBucket: "listen-together-637e4.appspot.com",
+  messagingSenderId: "434149085946",
+  appId: "1:434149085946:web:fd93437f9e7d8030c42f43",
+  measurementId: "G-BQFCMGHPDV",
+}
+
+firebase.initializeApp(config)
+firebase.analytics()
+
+export { firebase }
