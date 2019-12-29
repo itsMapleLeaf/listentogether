0f15f1fe4b2827f89b758f081dee520c4523e22d
kingdaro <kingdaro@gmail.com>
2019-12-28 21:44:38 -0600

    remove apollo stuff

--- workspaces/web/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    service: {
-      name: "api",
-      localSchemaFile: `${__dirname}/../api/src/generated/schema.graphql`,
-      includes: [`${__dirname}/src/**/*.graphql`],
-    },
-  },
-}
