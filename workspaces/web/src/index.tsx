6986be0c275ddefb21e312cf94e329b3f92f0859
kingdaro <kingdaro@gmail.com>
2019-12-31 00:20:52 -0600

    rubik!

--- workspaces/web/src/index.tsx
+++ workspaces/web/src/index.tsx
@@ -1,7 +1,12 @@
 import "focus-visible"
 import React from "react"
 import ReactDOM from "react-dom"
+import WebFont from "webfontloader"
 import App from "./app/App"
 import "./ui/style.css"
+
+WebFont.load({
+  google: { families: ["Rubik"] },
+})
 
 ReactDOM.render(<App />, document.getElementById("root"))
