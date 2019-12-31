79f2fcd17c33397d74670b4057a6dacd51625502
kingdaro <kingdaro@gmail.com>
2019-12-31 03:37:35 -0600

    add purgecss

--- workspaces/web/craco.config.js
+++ workspaces/web/craco.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+const glob = require("glob")
+const PurgeCssPlugin = require("purgecss-webpack-plugin")
+
+module.exports = ({ env }) => {
+  const config = {
+    style: {
+      postcss: { plugins: [require("tailwindcss")] },
     },
-  },
+    webpack: { plugins: [] },
+  }
+
+  if (env === "production") {
+    config.webpack.plugins.push(
+      new PurgeCssPlugin({
+        paths: glob.sync(`@(src|public)/**/*`, { nodir: true }),
+      }),
+    )
+    config.style.postcss.plugins.push(require("autoprefixer"))
+  }
+
+  return config
 }
